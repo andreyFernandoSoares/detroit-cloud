4ecd0cc2f2f6cebeaf9337885425a0ca134bf55b
diddyzin <andreysykez19@gmail.com>
2020-08-11 16:47:50 -0300

    Alterar nome class main

--- store/src/main/java/br/com/detroit/store/StoreApplication.java
+++ store/src/main/java/br/com/detroit/store/StoreApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DetroitCloudApplication {
+public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DetroitCloudApplication.class, args);
+		SpringApplication.run(StoreApplication.class, args);
 	}
 }
